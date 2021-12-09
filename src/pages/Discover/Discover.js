44ebc64b34ea25b71aec302f64ff13b90e27c6ec
erik G <egomezfax19@gmail.com>
2021-12-08 21:06:19 -0800

    discover page basic functionality done

--- src/pages/Discover/Discover.js
+++ src/pages/Discover/Discover.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Row, Col, Figure } from "react-bootstrap";
+import { Row, Col, Figure, Alert } from "react-bootstrap";
 import API from "../../utils/API";
 import "./style.css";
 import TinderButton from "../../components/Button/Button";
@@ -8,6 +8,7 @@ class Discover extends Component {
 	state = {
 		newDog: "",
 		newFriend: 0,
+		dogMatch: false,
 	};
 
 	componentDidMount() {
@@ -15,28 +16,27 @@ class Discover extends Component {
 	}
 
 	handleClick = (event) => {
-		// if button yes is clicked the 1 in 5 will become new friend
+		if (this.state.dogMatch === true) {
+			this.setState({ dogMatch: false });
+		}
+
 		if (event.target.getAttribute("value") === "Yes") {
 			let newbies = this.state.newFriend;
 			const count = Math.random() * 5;
 			const match = () => {
 				if (count > 3) {
-					this.setState({ newFriend: newbies + 1 });
+					this.setState({ newFriend: newbies + 1, dogMatch: true });
 				}
 			};
 			match();
-			// const newState = { ...this.state };
 
-			// console.log(newState);
-
-			// this.setState(newState);
-
-			console.log(this.state);
+			// console.log(this.state);
 
 			this.getNewDog();
 		} else {
 			this.getNewDog();
 		}
+		console.log(this.state);
 	};
 
 	getNewDog = () => {
@@ -49,10 +49,11 @@ class Discover extends Component {
 	};
 
 	render() {
+		const perfectMatch = this.state.dogMatch;
 		return (
 			<Row className="image_position">
 				<Col xs={6}>
-					<Figure>
+					<Figure className="spacing">
 						<Figure.Image
 							src={this.state.newDog}
 							alt="dog image"
@@ -61,7 +62,7 @@ class Discover extends Component {
 						/>
 					</Figure>
 
-					<section className="button_position">
+					<section className="button_position spacing">
 						<TinderButton
 							variant="danger"
 							use="No"
@@ -76,6 +77,15 @@ class Discover extends Component {
 						>
 							Yes
 						</TinderButton>
+					</section>
+					<section className="spacing">
+						{perfectMatch === true ? (
+							<Alert variant="success">
+								<p>Nice! This dog likes the way you smell!</p>
+							</Alert>
+						) : (
+							<br />
+						)}
 					</section>
 				</Col>
 			</Row>
