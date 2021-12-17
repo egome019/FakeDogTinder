e9f77cb8d00b36cb7218c3926b8708b00f821ccd
erik G <egomezfax19@gmail.com>
2021-12-16 21:59:44 -0800

    made skeleton discover page and started to mount state, not functional yet

---
+++ src/components/Card Group/Cards.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Cards() {
+	return (
+		<section>
+			<p> Cards go here</p>
+		</section>
+	);
+}
+
+export default Cards;
--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,15 +1,42 @@
 import React, { Component } from "react";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Form } from "react-bootstrap";
+import Cards from "../../components/Card Group/Cards";
+import API from "../../utils/API";
 import "./style.css";
 
 class Search extends Component {
+	state = {
+		newBreed: "",
+		breedList: [],
+	};
+
+	componentDidMount() {
+		API.breedList()
+			.then((response) => {
+				this.setState({ breedList: response.data.message });
+			})
+			.catch((error) => console.log(error));
+	}
+
+	getNewBreed = () => {
+		// API. based on the breed you select then you map for the cards
+	};
+
 	render() {
 		return (
 			<Row className="container_flex">
 				<Col xs={6}>
-					<section className="spacing header_center">
+					<section className="spacing header_center ">
 						<h1>Search by Breed!</h1>
 					</section>
+					<section>
+						<Form.Select aria-label="Breed Select">
+							{/* {this.state.breedList.map((breed) => {
+								return <option key={breed.id}>{breed}</option>;
+							})} */}
+						</Form.Select>
+					</section>
+					<Cards />
 				</Col>
 			</Row>
 		);
